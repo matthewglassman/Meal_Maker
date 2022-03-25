5f16666ae5fc35a5063b2ec36e98969f72025380
Matthew Glassman <matthewglassman78@gmail.com>
2022-03-25 18:52:09 -0400

    Created a menu object with courses object which contains the course properties as arrays.

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //This app will select out a 3 course meal based on what is available on a menu.
+
+var menu = {
+    courses: {
+        appetizers: [],
+        mains: [],
+        desserts: []
+    },
+    
+}
